b894dd629b877fee045db0c6baed8b265d2d3733
hieutn.hdt <hieucot1998@gmail.com>
2025-01-23 02:24:53 +0700

    setup and create SplashFragment

--- app/src/main/java/com/hdt/qrscanner/ui/MainActivity.kt
+++ app/src/main/java/com/hdt/qrscanner/ui/MainActivity.kt
@@ -2,15 +2,12 @@ package com.hdt.qrscanner.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.hdt.qrscanner.databinding.ActivityMainBinding
+import com.hdt.qrscanner.R
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_main)
     }
 }
